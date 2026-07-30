f8c76659a565a6454df7c4be705d1015eae1f37b
joelsoh20 <sohjoel20@gmail.com>
2026-07-30 17:02:14 +0200

    Fix: annulation des consoles dans AuthService.ts

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -20,14 +20,14 @@ export class AuthService {
   }
 
   console.log('✅ Utilisateur trouvé:', user.nom);
-  console.log('🔑 Hash stocké:', user.mot_de_passe);
-  console.log('🔑 Hash attendu:', '$2a$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewY5GyFkR0NtVnUu');
-  console.log('Mot de passe reçu :', motDePasse);
-console.log('Hash en base :', user.mot_de_passe);
+  //console.log('🔑 Hash stocké:', user.mot_de_passe);
+  //console.log('🔑 Hash attendu:', '$2a$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewY5GyFkR0NtVnUu');
+  //console.log('Mot de passe reçu :', motDePasse);
+//console.log('Hash en base :', user.mot_de_passe);
 
 
   const valid = await user.verifierMotDePasse(motDePasse);
-  console.log('Résultat bcrypt.compare :', valid);
+  //console.log('Résultat bcrypt.compare :', valid);
   if (!valid) {
     return { 
       success: false, 
